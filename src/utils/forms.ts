097e15f95af2dd1ad2e1a12dbc252c34ff0a2edf
Denio Oliveira <denio.1838@gmail.com>
2024-03-27 15:12:01 -0300

    Dirty and validate all

--- src/utils/forms.ts
+++ src/utils/forms.ts
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 /* eslint-disable no-var */
 /* eslint-disable @typescript-eslint/no-explicit-any */
 
@@ -43,4 +44,41 @@ export function updateAndValidate(inputs: any, name: string, newValue: any){
 export function dirtyAndValidate(inputs: any, name: string){
     const dataDirty = toDirty(inputs, name);
     return validate(dataDirty, name);
+}
+
+export function toDirtyAll(inputs: any){
+    const newImputs: any = {};
+    for (var name in inputs){
+        newImputs[name] = { ...inputs[name], dirty: "true"};
+    }
+    return newImputs;
+}
+
+export function validateAll(inputs: any){
+    const newImputs: any = {};
+
+    for(var name in inputs){
+        if( inputs[name].validation){
+            const isInvalid = !inputs[name].validation(inputs[name].value);
+            newImputs[name] = { ...inputs[name], invalid: isInvalid.toString()};
+        }
+        else{
+            newImputs[name] = { ...inputs[name]}
+        }       
+    }
+
+    return newImputs;
+}
+
+export function dirtyAndValidateAll (inputs: any){
+    return validateAll(toDirtyAll(inputs));
+}
+
+export function hasAnyInvalid (inputs: any){
+    for (var name in inputs){
+        if(inputs[name].dirty === "true" && inputs[name].invalid ==="true"){
+            return true;
+        }
+    }
+    return false;
 }
